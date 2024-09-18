e2f72d6f9e76ac30ab243e17ff528edd4b2c771b
Alperen <alperen@example.com>
2024-09-18 17:33:06 +0200

    Bankomat_Beispiel Finished

--- src/Bankomat_Beispiel.java
+++ src/Bankomat_Beispiel.java
@@ -10,7 +10,55 @@ public class Bankomat_Beispiel {
         // 3. Kontostand
         // 4. Beenden
 
+        int balance = 0;
+        boolean isFinished = false;
+        Scanner scanner = new Scanner(System.in);
 
+        while (!isFinished){
+
+            System.out.println("1. Einzahlen");
+            System.out.println("2. Auszahlen");
+            System.out.println("3. Kontostand");
+            System.out.println("4. Beenden");
+            int selection = scanner.nextInt();
+
+            switch (selection){
+                case 1:
+                    System.out.println("Geben Sie den Betrag ein, der ausgezahlt werden soll: ");
+                    int einzahlen = scanner.nextInt();
+                    if (einzahlen > balance){
+                        balance += einzahlen;
+                        System.out.println(einzahlen + "€ wurden erfolgreich eingezahlt!");
+                    } else {
+                        System.out.println("Ungültiger Betrag!");
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Geben Sie den Betrag ein, der ausgezahlt werden soll: ");
+                    int auszahlen = scanner.nextInt();
+                    if (auszahlen > 0 && auszahlen <= balance){
+                        balance -= auszahlen;
+                        System.out.println(auszahlen + "€ wurden erfolgreich ausgezahlt!");
+                    } else if (auszahlen > balance) {
+                        System.out.println("Nicht genügend Guthaben! Arbeiten sie härter!");
+                    } else {
+                        System.out.println("Ungültiger Betrag!");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println(balance + "€ befinden sich auf dem Konto");
+                    break;
+
+                case 4:
+                    System.out.println("Vielen dank für ihren Besuch!");
+                    isFinished = true;
+                    break;
+            }
+
+
+        }
 
 
     }
