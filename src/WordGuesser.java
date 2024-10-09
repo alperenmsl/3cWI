9057c3b7f84c6bda9587a2a0f3297f123d7ae465
Alperen <alperen@example.com>
2024-10-09 09:33:09 +0200

    Word guesser Finished

--- src/WordGuesser.java
+++ src/WordGuesser.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+
 public class WordGuesser {
     public static void main(String[] args) {
         // Word guesser = Wörter erraten zbs. H*ll* = Hallo
         // If schleife -> If word = Hallo gibt correct aus. Wenn nicht dann false.
 
-        String word = "Hallo";
+        Scanner scanner = new Scanner(System.in);
+        String word = "";
+        boolean isFinished = false;
+
+        while (!isFinished) {
+            System.out.println("Gues the word H**l*");
+            word = scanner.next();
+
+            if (word.equals("Hallo")) {
+                System.out.println("You guess the correct! It was " + word);
+                isFinished = true;
+            } else {
+                System.out.println("You didn't guess the word right please try it again.");
+            }
+
+
+        }
 
 
     }
