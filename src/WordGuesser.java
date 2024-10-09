ca1eab2d6b41c8106da47326de0c925a6491b9d0
Alperen <alperen@example.com>
2024-10-09 08:44:01 +0200

    WordGuesser

---
+++ src/WordGuesser.java
@@ -0,0 +1,10 @@
+public class WordGuesser {
+    public static void main(String[] args) {
+        // Word guesser = Wörter erraten zbs. H*ll* = Hallo
+        // If schleife -> If word = Hallo gibt correct aus. Wenn nicht dann false.
+
+        String word = "Hallo";
+
+
+    }
+}
