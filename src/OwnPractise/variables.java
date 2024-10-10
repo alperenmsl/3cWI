0d22b3c19180b8785c4f2fdb4959c6c326bbac97
Alperen <alperen@example.com>
2024-10-10 09:25:01 +0200

    End of lesson

---
+++ src/OwnPractise/variables.java
@@ -0,0 +1,34 @@
+package OwnPractise;
+
+public class variables {
+    public static void main(String[] args) {
+        //String - stores text, such as "Hello". String values are surrounded by double quotes
+        //int - stores integers (whole numbers), without decimals, such as 123 or -123
+        //float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+        //char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+        //boolean - stores values with two states: true or false
+
+        String name = "Alperen Meseli";
+        int age = 16;
+        float height = 1.70f;
+        char instagram = '@';
+        boolean isBig = true;
+
+        System.out.println("My name is "+name+" and my age is "+age+" and my height is "+height+". My name on Instagram is "+instagram+"alper3n.");
+
+
+//      ---  Primitive Data Types ---
+
+        //Data Type
+        //byte	  1 byte	Stores whole numbers from -128 to 127
+        //short	  2 bytes	Stores whole numbers from -32,768 to 32,767
+        //int	  4 bytes	Stores whole numbers from -2,147,483,648 to 2,147,483,647
+        //long	  8 bytes	Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        //float	  4 bytes	Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+        //double  8 bytes	Stores fractional numbers. Sufficient for storing 15 decimal digits
+        //boolean 1 bit	    Stores true or false values
+        //char	  2 bytes	Stores a single character/letter or ASCII values
+
+
+    }
+}
