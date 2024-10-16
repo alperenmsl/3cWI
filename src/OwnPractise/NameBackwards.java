54ddcd7cd176f645e17260236fe39ad2aee94650
Alperen <alperen@example.com>
2024-10-16 08:03:10 +0200

    Start lesson

---
+++ src/OwnPractise/NameBackwards.java
@@ -0,0 +1,10 @@
+package OwnPractise;
+
+public class NameBackwards {
+    public static void main(String[] args) {
+        String name = "Alperen";
+        for (int i = 0; i > name.length(); i--) {
+            System.out.println(name);
+        }
+    }
+}
