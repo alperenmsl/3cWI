84226950ef3a64fcbf7e8697ad40beb81dc324e2
Alperen <alperen@example.com>
2024-10-17 09:29:55 +0200

    String Helper Finished

---
+++ src/StringHelper.java
@@ -0,0 +1,56 @@
+public class StringHelper {
+    public static void main(String[] args) {
+        // Is Palindrome (String): boolean
+        // countLetters (String, Char): int
+        // reverseString (String): String
+        // printAmountOfLetters (String)
+
+        // Palindrome
+        boolean isPalindrome = isPalindrome("anna");
+        if (isPalindrome) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
+        // countLetters
+        int amount = countLetters("alperen", 'a');
+        System.out.println(amount);
+
+        // reverseString
+        String reversed = reverseString("Messi");
+
+        // AmountOfLetters
+        int letters = AmountOfLetters("Hassan");
+        System.out.println("\n");
+        System.out.println(letters);
+
+    }
+
+    public static int AmountOfLetters(String word){
+        return word.length();
+    }
+
+
+    public static String reverseString(String string){
+        for (int i = string.length() -1; i >= 0; i--) {
+            System.out.print(string.charAt(i));
+        }
+        return string;
+    }
+
+    public static boolean isPalindrome(String word) {
+        String reversed = new StringBuilder(word).reverse().toString();
+        return word.equals(reversed);
+    }
+
+    public static int countLetters(String word, char c) {
+        int letters = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == c) {
+                letters++;
+            }
+        }
+        return letters;
+    }
+}
---
+++ src/WordGuesserV2.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class WordGuesserV2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String word = "Alperen";
+        char[] wordToGuess = word.toCharArray();
+        char[] guessedWord = new char[word.length()];
+
+        for (int i = 0; i < word.length(); i++) {
+            guessedWord[i] = '*';
+        }
+
+        boolean wordGuessed = false;
+        int attemptsLeft = 5;
+
+        while (!wordGuessed && attemptsLeft > 0) {
+            System.out.println("Aktuelles Wort: ");
+            printAray(guessedWord);
+            System.out.println("\nRate einen Buchstaben: ");
+            char guess = scanner.next().charAt(0);
+
+            boolean correctGuess = false;
+
+            for (int i = 0; i < wordToGuess.length; i++) {
+                if (wordToGuess[i] == guess) {
+                    guessedWord[i] = guess;
+                    correctGuess = true;
+                }
+            }
+
+            if (!correctGuess) {
+                attemptsLeft--;
+                System.out.println("Falsch. Verbleibende Versuche: " + attemptsLeft);
+            }
+
+            if (String.valueOf(guessedWord).equals(word)) {
+                wordGuessed = true;
+            }
+        }
+
+        if (wordGuessed) {
+            System.out.println("Du hast das Wort richtig geraten: " + word + "!");
+        } else {
+            System.out.println("Leider verloren! Das Wort war: " + word);
+        }
+
+        scanner.close();
+    }
+
+
+    public static void printAray(char[] data){
+        for (char c : data){
+            System.out.print(c);
+        }
+    }
+}
