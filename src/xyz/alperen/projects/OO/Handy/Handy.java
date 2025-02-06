ea1e6234ba9b16b5eb959b44803be6fee8c52634
Alperen <alperen@example.com>
2025-02-06 08:37:50 +0100

    Was ist final

--- src/xyz/alperen/projects/OO/Handy/Handy.java
+++ src/xyz/alperen/projects/OO/Handy/Handy.java
@@ -2,4 +2,21 @@ package xyz.alperen.projects.OO.Handy;
 
 public class Handy {
     private String color;
+
+    public void takePicture(){
+        System.out.println("Taking picture");
+    }
+
+    public void makeCall(String makeCall){
+        System.out.println("Making a call");
+    }
+
+    public void getFreeSpace(int freeSpace){
+        System.out.println("Get free space");
+    }
+
+    public void printAllFiles(){
+        System.out.println("Printing all files");
+    }
+
 }
---
+++ src/xyz/alperen/projects/OO/Handy/Sim.java
@@ -0,0 +1,7 @@
+package xyz.alperen.projects.OO.Handy;
+
+public class Sim {
+    private int id;
+    private String number;
+
+}
